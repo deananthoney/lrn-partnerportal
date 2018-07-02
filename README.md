e018218c658676be283cd0c2ad6e2eb8b4903a15
Dean Rose <moneymint23@yahoo.com>
2018-07-02 12:16:10 -0400

    message

---
+++ README.md
@@ -0,0 +1,7 @@
+# lrn-partnerportal
+
+
+
+HTML,CSS,Boostrap,Angular JS,etc
+
+Deveeloped by Dean Rose
---
+++ js/common.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+//$('.navbar-toggle').removeClass('toggleOpen');
+$('.navbar-toggle').on('click', function(){
+  	$( this ).toggleClass('toggleOpen');
+});
